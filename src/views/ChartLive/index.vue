963ff99ab496a21e1c695a693e6d556f439b9d17
Ibnu <ibnu.meiriyanto@btpn.com>
2020-07-18 11:29:25 +0700

    add git

---
+++ src/views/ChartLive/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <base-header type="gradient-success" class="pb-6 pb-8 pt-5 pt-md-8"></base-header>
+
+    <div class="container-fluid mt--7">
+      <div class="row">
+        <div class="col">
+            <List/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import List from './list.vue'
+export default {
+    name: 'live-chart',
+    components: {
+        List
+    }
+}
+</script>
---
+++ src/views/ChartLive/list.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <b-card class="mb-4">
+      <b-table striped hover :items="items"></b-table>
+    </b-card>
+  </div>
+</template>
+<script>
+export default {
+  name: "list",
+  props: {},
+  data() {
+    return {
+      items: [
+        { age: 40, first_name: "Dickerson", last_names: "Macdonald" },
+        { age: 21, first_name: "Larsen", last_names: "Shaw" },
+        { age: 89, first_name: "Geneva", last_names: "Wilson" },
+        { age: 38, first_name: "Jami", last_names: "Carney" }
+      ]
+    };
+  }
+};
+</script>
